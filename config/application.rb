34b2bcab7123b703a50f212c075652a4225e8ba5
Timothy <lemayiant4@gmail.com>
2022-11-28 14:20:41 +0300

    completed assignment

--- config/application.rb
+++ config/application.rb
@@ -24,17 +24,7 @@ module Phase4CookiesInRailsApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
     # Adding back cookies and session middleware
@@ -43,5 +33,8 @@ module Phase4CookiesInRailsApi
 
     # Use SameSite=Strict for all cookies to help protect against CSRF
     config.action_dispatch.cookies_same_site_protection = :strict
+  end
+  class ApplicationController < ActionController::API
+    include ActionController::Cookies
   end
 end
